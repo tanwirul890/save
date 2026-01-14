8a203cd64ba78a3114d79a46cffc7efa7728cf7e
tanwirul890 <tanrul890@gmail.com>
2026-01-14 12:28:44 +0700

    ssss

--- layout/header/header-user.php
+++ layout/header/header-user.php
@@ -30,7 +30,7 @@ $halaman = basename($_SERVER['REQUEST_URI']);
 
 body {
     font-family: 'Poppins';
-    background: #fecaca;
+    background: #ededed;
 }
 
 /* ================= BURGER MENU ================= */
